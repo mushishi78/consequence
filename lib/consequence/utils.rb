cfbb271aa8ae546cd697defafb13a3d805e64254
Max White <mushishi78@gmail.com>
2015-01-14 19:12:06 +0000

    Utils: type_check and indentation

--- lib/consequence/utils.rb
+++ lib/consequence/utils.rb
@@ -1,11 +1,15 @@
 module Consequence
-	module Utils
-		def send_to_value(*args)
-			->(v) { v.send(*args) }
-		end
+  module Utils
+    def send_to_value(*args)
+      ->(v) { v.send(*args) }
+    end
 
-		def send_to_monad(*args)
-			->(v, m) { m.send(*args) }
-		end
-	end
+    def send_to_monad(*args)
+      ->(v, m) { m.send(*args) }
+    end
+
+    def type_check(*args)
+      ->(v) { raise TypeError unless args.include?(v.class) }
+    end
+  end
 end
