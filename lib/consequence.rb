43f149569f7e014be2b351bb3e157e405a47410c
Max White <mushishi78@gmail.com>
2015-01-16 05:21:29 +0000

    And pull the require to eventually too..

--- lib/consequence.rb
+++ lib/consequence.rb
@@ -4,6 +4,5 @@ end
 require 'consequence/monad'
 require 'consequence/success'
 require 'consequence/something'
-require 'consequence/eventually'
 require 'consequence/delegates_to_value'
 require 'consequence/utils'
